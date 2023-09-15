ce3937ea53a77c3f86fcd9bf92ac8efc259723c8
ZerXDE "Till O <blackminer1242@gmail.com>
2023-09-15 21:52:47 +0200

    uodate

--- extension.js
+++ extension.js
@@ -1,10 +1,18 @@
 const vscode = require("vscode");
-var Rcon = require("rcon");
+const Rcon = require("rcon");
 const path = require("path");
 
 let conn;
 
-function disconnect() {
+function showErrorMessage(message) {
+  vscode.window.showErrorMessage(message);
+}
+
+function showInfoMessage(message) {
+  vscode.window.showInformationMessage(message);
+}
+
+function disconnectFromServer() {
   if (conn) {
     conn.disconnect();
     conn = null;
@@ -12,51 +20,82 @@ function disconnect() {
   }
 }
 
-function connnect(password, ip, port) {
-  console.log(password, ip, port);
-  if (!ip && !port) {
-    ip = "127.0.0.1";
-    port = "30120";
-  }
-  disconnect();
-  conn = new Rcon(ip, port, password, {
-    tcp: false,
-    challenge: false,
-  });
-
+function handleRconEvents() {
   conn
-    .on("auth", function () {
+    .on("auth", () => {
       console.log("Authenticated");
       conn.send("refresh");
     })
-    .on("response", function (str) {
+    .on("response", (str) => {
       console.log(str);
-
       if (str === "rint Invalid password") {
-        vscode.window.showErrorMessage("Invalid password");
+        showErrorMessage("Invalid password");
       }
-
-      if (str.indexOf("rint ^2Scanning resources.^7") >= 0) {
-        vscode.window.showInformationMessage("Connected");
+      if (str.includes("rint ^2Scanning resources.^7")) {
+        showInfoMessage("Connected");
       }
     })
-    .on("error", function (err) {
-      console.log("Error: " + err);
-      vscode.window.showErrorMessage("Error: " + err);
+    .on("error", (err) => {
+      showErrorMessage(`Error: ${err}`);
     })
-    .on("end", function () {
-      console.log("Connection closed");
-      vscode.window.showErrorMessage("Connection closed");
+    .on("end", () => {
+      showErrorMessage("Connection closed");
       process.exit();
     });
+}
 
+function connectToServer(password, ip = "127.0.0.1", port = "30120") {
+  disconnectFromServer();
+  conn = new Rcon(ip, port, password, { tcp: false, challenge: false });
+  handleRconEvents();
   conn.connect();
 }
 
-/**
- * @param {vscode.ExtensionContext} context
- */
+function safeConnect(password, ip, port) {
+  if (conn) {
+    vscode.window.showWarningMessage("Already connected. Disconnect first.");
+    return;
+  }
+  connectToServer(password, ip, port);
+  vscode.workspaceState.update("easy-fivem-connection", { password, ip, port });
+}
+
+function connectToSaved() {
+  const savedConnection = vscode.workspaceState.get("easy-fivem-connection");
+  if (!savedConnection) {
+    showErrorMessage("No saved connection found.");
+    return;
+  }
+  safeConnect(savedConnection.password, savedConnection.ip, savedConnection.port);
+}
+
+function setCurrentResource(folder) {
+  if (!folder || !folder.fsPath) {
+    showErrorMessage("Invalid resource folder selected!");
+    return;
+  }
+  const folderName = path.basename(folder.fsPath);
+  if (conn) {
+    conn.send(`ensure ${folderName}`);
+    showInfoMessage(`Set ${folderName} as the current resource.`);
+  } else {
+    showErrorMessage("Please connect to a server first.");
+  }
+}
+
 function activate(context) {
+  // Check for saved connection at startup
+  const savedConnection = vscode.workspaceState.get("easy-fivem-connection");
+  if (savedConnection) {
+    vscode.window.showInformationMessage("Found saved FiveM connection. Use 'Connect to Saved Connection' to connect.", "Connect")
+      .then(selection => {
+        if (selection === "Connect") {
+          connectToSaved();
+        }
+      });
+  }
+
+  // Event: Save Text Document
   vscode.workspace.onDidSaveTextDocument((document) => {
     if (document.uri.scheme === "file" && conn) {
       vscode.workspace.workspaceFolders.forEach((folder) => {
@@ -65,74 +104,51 @@ function activate(context) {
     }
   });
 
-  let connect = vscode.commands.registerCommand(
-    "easy-fivem.connect",
-    async function () {
-      const password = await vscode.window.showInputBox({
-        placeHolder: "password",
-        prompt: "your server rcon password",
-      });
-      if (password && password !== "") {
-        connnect(password);
-      } else {
-        vscode.window.showErrorMessage("no valid password!");
-      }
+  // Command: Connect
+  let connectCommand = vscode.commands.registerCommand('easy-fivem.connect', async function () {
+    const password = await vscode.window.showInputBox({ placeHolder: "password", prompt: "Your server RCON password" });
+    if (password) {
+      safeConnect(password);
+    } else {
+      showErrorMessage("No valid password provided!");
     }
-  );
+  });
 
-  context.subscriptions.push(connect);
+  // Command: Disconnect
+  let disconnectCommand = vscode.commands.registerCommand('easy-fivem.disconnect', disconnectFromServer);
 
-  let disconnect = vscode.commands.registerCommand(
-    "easy-fivem.disconnect",
-    function () {
-      if (conn) {
-        conn.disconnect();
-        conn = null;
-        vscode.window.showWarningMessage("Disconnected");
-      }
+  // Command: Custom Connect
+  let customConnectCommand = vscode.commands.registerCommand('easy-fivem.customConnect', async function () {
+    let connectionDetails = await vscode.window.showInputBox({ placeHolder: "ip:port", prompt: "Your server IP and port" });
+    let [ip, port] = connectionDetails.split(":");
+    const password = await vscode.window.showInputBox({ placeHolder: "password", prompt: "Your server RCON password" });
+
+    if (password && ip && port) {
+      safeConnect(password, ip, port);
+    } else {
+      showErrorMessage("Incomplete connection details provided!");
     }
+  });
+
+  // Command: Connect to Saved
+  let connectSavedCommand = vscode.commands.registerCommand('easy-fivem.connectSaved', connectToSaved);
+
+  // Command: Set Current Resource
+  let setCurrentResourceCommand = vscode.commands.registerCommand('easy-fivem.setCurrentResource', setCurrentResource);
+
+  // Subscriptions
+  context.subscriptions.push(
+    connectCommand,
+    disconnectCommand,
+    customConnectCommand,
+    connectSavedCommand,
+    setCurrentResourceCommand
   );
-
-  context.subscriptions.push(disconnect);
-
-  let customConnect = vscode.commands.registerCommand(
-    "easy-fivem.customConnect",
-    async function () {
-      let port;
-      let ip = await vscode.window.showInputBox({
-        placeHolder: "ip:port",
-        prompt: "your server ip and port",
-      });
-
-      port = ip.split(":")[1];
-      ip = ip.split(":")[0];
-
-      const password = await vscode.window.showInputBox({
-        placeHolder: "password",
-        prompt: "your server rcon password",
-      });
-      if (
-        password &&
-        password !== "" &&
-        ip &&
-        ip !== "" &&
-        port &&
-        port !== ""
-      ) {
-        connnect(password, ip, port);
-      } else {
-        vscode.window.showErrorMessage("no valid password!");
-      }
-    }
-  );
-
-  context.subscriptions.push(customConnect);
 }
 
-// this method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
   activate,
-  deactivate,
+  deactivate
 };
